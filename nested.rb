0cac6d3ecc3e4ee92e891d63c5e46ec14391afb7
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-20 23:52:38 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,3 +1,22 @@
+def hopper
+	programmer_hash = 
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+
+
+end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -41,7 +60,7 @@ def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
-#    :languages => ["LISP, C"]
+#    :languages => ["LISP", "C"]
 # }
 
 	programmer_hash = 
